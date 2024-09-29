311366453b184c914d23dde2b1ceeea20185b921
Omar <nyirurugoomar@gmail.com>
2024-09-30 00:45:08 +0200

    Creating hero components

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  avatars = [
+    'https://i.pravatar.cc/150?u=a042581f4e29026024d',
+    'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+    'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+    'https://i.pravatar.cc/150?u=a04258114e29026302d',
+    'https://i.pravatar.cc/150?u=a04258114e29026702d'
+  ];
+  maxAvatars = 5;
 }
