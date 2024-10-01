d6c666848f9049c9435b6f7f4cbe1c368e518595
Omar <nyirurugoomar@gmail.com>
2024-10-01 16:44:26 +0200

    Creating Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { HeroComponent } from './components/hero/hero.component';
 import { FeaturesComponent } from './components/features/features.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { ConnectComponent } from './components/connect/connect.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeroComponent,FeaturesComponent,ReviewsComponent,ConnectComponent],
+  imports: [RouterOutlet, HeaderComponent,HeroComponent,FeaturesComponent,ReviewsComponent,ConnectComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
